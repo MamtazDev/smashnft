a7c6bc2604c300edec6520afb6c8cd0b9d7c57ba
nipaaa <akternipa941@gmail.com>
2023-03-07 18:31:00 -0800

    update

---
+++ src/pages/ecommerce/ContractApplication.jsx
@@ -0,0 +1,194 @@
+import React, { useState } from "react";
+import DateSelect from "../../components/DateSelect";
+import PaginationClassic from "../../components/PaginationClassic";
+import Header from "../../partials/Header";
+import Sidebar from "../../partials/Sidebar";
+
+const ContractApplication = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleSelectedItems = (selectedItems) => {
+    setSelectedItems([...selectedItems]);
+  };
+  return (
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      {/* Content area */}
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+        {/*  Site header */}
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        <main>
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+            {/* Page header */}
+            <div className="sm:flex sm:justify-between sm:items-center mb-8">
+              {/* Left: Title */}
+              <div className="mb-4 sm:mb-0">
+                <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">
+                  Contract Application ✨
+                </h1>
+              </div>
+
+              {/* Right: Actions */}
+              <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+                {/* Delete button */}
+                {/* <DeleteButton selectedItems={selectedItems} /> */}
+                {/* Dropdown */}
+                <DateSelect />
+                {/* Filter button */}
+                {/* <FilterButton align="right" /> */}
+                {/* Add customer button */}
+                <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
+                  <svg
+                    className="w-4 h-4 fill-current opacity-50 shrink-0"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                  </svg>
+                  <span className="hidden xs:block ml-2">Add Order</span>
+                </button>
+              </div>
+            </div>
+
+            {/* Table */}
+            {/* Table */}
+            <div className="overflow-x-auto">
+              <table className="table-auto w-full divide-y divide-slate-200">
+                {/* Table header */}
+                <thead className="text-xs uppercase text-slate-500 bg-slate-50 border-t border-slate-200">
+                  <tr>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
+                      <div className="flex items-center">
+                        <label className="inline-flex">
+                          <span className="sr-only">Select all</span>
+                          <input className="form-checkbox" type="checkbox" />
+                        </label>
+                      </div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">
+                        Application user
+                      </div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">
+                        Contract name
+                      </div>
+                    </th>
+
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">
+                        Contract type
+                      </div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">Main chain</div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">
+                        Initial block
+                      </div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">E-mail</div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold">Contract address</div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">state</div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">我不知道</div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">Reviewer</div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">Audit time</div>
+                    </th>
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">Create time</div>
+                    </th>
+
+                    <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="font-semibold text-left">Operation</div>
+                    </th>
+                  </tr>
+                </thead>
+
+                {/* Table body */}
+                <tbody className="text-sm">
+                  <tr>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
+                      <div className="flex items-center">
+                        <label className="inline-flex">
+                          <span className="sr-only">Select</span>
+                          <input className="form-checkbox" type="checkbox" />
+                        </label>
+                      </div>
+                    </td>
+
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="text-red-600">0xjhhg...jhgg</div>
+                    </td>
+
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>blank</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>ERC1155</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>BSCTest</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>154258896</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>blank</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="text-red-600">0xjhhg...jhgg</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="bg-green-200 p-2 rounded-lg">
+                        Approved
+                      </div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>1</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>admin</div>
+                    </td>
+
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>2022-01-25</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div>2022-01-25</div>
+                    </td>
+                    <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                      <div className="text-blue-600">Edit Delete</div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            {/* Pagination */}
+            <div className="mt-8">
+              <PaginationClassic />
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default ContractApplication;
