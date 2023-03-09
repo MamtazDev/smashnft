7704d2a1110409d0106fdbb78835391264417f93
nipaaa <akternipa941@gmail.com>
2023-03-09 11:53:51 -0800

    support wallet adda

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -455,7 +455,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                              Mail configuration
+                                Mail configuration
                               </span>
                             </NavLink>
                           </li>
