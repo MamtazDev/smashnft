126b2b6744953e657155a8c97fd3d2bf57dfdfde
nipaaa <akternipa941@gmail.com>
2023-03-09 12:22:02 -0800

    role management add

--- src/App.jsx
+++ src/App.jsx
@@ -103,6 +103,7 @@ import CarouselPictures from "./pages/ecommerce/CarouselPictures";
 import MailConfiguration from "./pages/ecommerce/MailConfiguration";
 import SupportWallet from "./pages/ecommerce/SupportWallet";
 import Administrator from "./pages/ecommerce/Administrator";
+import RoleManagement from "./pages/ecommerce/RoleManagement";
 
 
 
@@ -149,6 +150,7 @@ function App() {
         <Route path="/ecommerce/contract-transaction" element={<ContractTransaction />} />
         <Route path="/ecommerce/contract-application" element={<ContractApplication />} />
         <Route path="/ecommerce/administrator" element={<Administrator />} />
+        <Route path="/ecommerce/role-management" element={<RoleManagement />} />
         <Route path="/ecommerce/transaction-currency" element={<TransactionCurrency />} />
         <Route path="/ecommerce/language-management" element={<LanguageManagement />} />
         <Route path="/ecommerce/notification-types" element={<NotificationTypes />} />
--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -441,7 +441,21 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                              Administrator
+                                Administrator
+                              </span>
+                            </NavLink>
+                          </li>
+                          <li className="mb-1 last:mb-0">
+                            <NavLink
+                              end
+                              to="/ecommerce/role-management"
+                              className={({ isActive }) =>
+                                "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
+                                (isActive ? "!text-indigo-500" : "")
+                              }
+                            >
+                              <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                Role Management
                               </span>
                             </NavLink>
                           </li>
