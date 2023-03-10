13967f19e70c497e042aded743dc00c902768d5c
nipaaa <akternipa941@gmail.com>
2023-03-09 16:01:28 -0800

    administrator and supported wallet filter input add

--- src/pages/ecommerce/Administrator.jsx
+++ src/pages/ecommerce/Administrator.jsx
@@ -5,7 +5,7 @@ import Header from "../../partials/Header";
 import Sidebar from "../../partials/Sidebar";
 
 const Administrator = () => {
-    const [comments, setComments] = useState(true);
+  const [comments, setComments] = useState(true);
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [selectedItems, setSelectedItems] = useState([]);
 
@@ -25,23 +25,58 @@ const Administrator = () => {
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
             {/* Page header */}
-            <div className="sm:flex sm:justify-between sm:items-center mb-8">
+            <div className="sm:flex flex-wrap gap-8 sm:justify-between sm:items-center mb-8">
               {/* Left: Title */}
               <div className="mb-4 sm:mb-0">
                 <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">
-                  Contract Application ✨
+                  Administrator ✨
                 </h1>
+              </div>
+              <div>
+                <label className="mr-2" for="status">
+                  Name
+                </label>
+                <input
+                  className="border-none"
+                  type="text"
+                  placeholder="please enter user name"
+                />
+              </div>
+              <div>
+                <label className="mr-2" for="status">
+                  Phone number
+                </label>
+                <input
+                  className="border-none"
+                  type="tel"
+                  placeholder="please enter your mobile phone no"
+                />
+              </div>
+
+              <div>
+                <label className="mr-2" for="status">
+                  Status
+                </label>
+                <select className="border-none" name="status" id="status">
+                  <option>User Status</option>
+                  <option>A</option>
+                  <option>B</option>
+                  <option>C</option>
+                </select>
+              </div>
+              <div>
+                <label className="mr-2" for="status">
+                  Creation time
+                </label>
+                <input
+                  className="border-none"
+                  type="date"
+                  placeholder="please enter role name"
+                />
               </div>
 
               {/* Right: Actions */}
               <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-                {/* Delete button */}
-                {/* <DeleteButton selectedItems={selectedItems} /> */}
-                {/* Dropdown */}
-                <DateSelect />
-                {/* Filter button */}
-                {/* <FilterButton align="right" /> */}
-                {/* Add customer button */}
                 <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
                   <svg
                     className="w-4 h-4 fill-current opacity-50 shrink-0"
@@ -49,7 +84,7 @@ const Administrator = () => {
                   >
                     <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                   </svg>
-                  <span className="hidden xs:block ml-2">Add Order</span>
+                  <span className="hidden xs:block ml-2">Newly added</span>
                 </button>
               </div>
             </div>
@@ -114,7 +149,6 @@ const Administrator = () => {
                       </div>
                     </td>
 
-
                     <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                       <div>1</div>
                     </td>
@@ -128,31 +162,29 @@ const Administrator = () => {
                       <div>014...</div>
                     </td>
                     <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
-                  <div className="flex items-center ml-4">
-                    <div className="text-sm text-slate-400 italic mr-2">
-                      {comments ? "On" : "Off"}
-                    </div>
-                    <div className="form-switch">
-                      <input
-                        type="checkbox"
-                        id="comments"
-                        className="sr-only"
-                        checked={comments}
-                        onChange={() => setComments(!comments)}
-                      />
-                      <label className="bg-slate-400" htmlFor="comments">
-                        <span
-                          className="bg-white shadow-sm"
-                          aria-hidden="true"
-                        ></span>
-                        <span className="sr-only">Enable smart sync</span>
-                      </label>
-                    </div>
-                  </div>
-                </td>
-                    
-                  
-                   
+                      <div className="flex items-center ml-4">
+                        <div className="text-sm text-slate-400 italic mr-2">
+                          {comments ? "On" : "Off"}
+                        </div>
+                        <div className="form-switch">
+                          <input
+                            type="checkbox"
+                            id="comments"
+                            className="sr-only"
+                            checked={comments}
+                            onChange={() => setComments(!comments)}
+                          />
+                          <label className="bg-slate-400" htmlFor="comments">
+                            <span
+                              className="bg-white shadow-sm"
+                              aria-hidden="true"
+                            ></span>
+                            <span className="sr-only">Enable smart sync</span>
+                          </label>
+                        </div>
+                      </div>
+                    </td>
+
                     <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
                       <div>2022-01-25</div>
                     </td>
