ed102a6d2a2fd23709835a9594e73d6916f33b51
nipaaa <akternipa941@gmail.com>
2023-03-09 19:21:49 -0800

    update royalty

--- src/pages/ecommerce/OfficialProducts.jsx
+++ src/pages/ecommerce/OfficialProducts.jsx
@@ -33,6 +33,34 @@ const OfficialProducts = () => {
                   Official Products ✨
                 </h1>
               </div>
+              <div>
+                <label className="mr-2" for="status">
+                  Chain
+                </label>
+                <select className="border-none" name="status" id="status">
+                  <option>Please select the main chain</option>
+                  <option>Japan</option>
+                  <option>Bangla</option>
+                </select>
+              </div>
+             
+              <div>
+                <label className="mr-2" for="status">
+                  Name
+                </label>
+                <input className="border-none" type="text" placeholder="Please enter a name" />
+              </div>
+              <div>
+                <label className="mr-2" for="status">
+                  Status
+                </label>
+                <select className="border-none" name="status" id="status">
+                  <option>Please select a status</option>
+                  <option>Japan</option>
+                  <option>Bangla</option>
+                </select>
+              </div>
+            
 
               {/* Right: Actions */}
               <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
@@ -50,7 +78,7 @@ const OfficialProducts = () => {
                   >
                     <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                   </svg>
-                  <span className="hidden xs:block ml-2">Add Order</span>
+                  <span className="hidden xs:block ml-2">Newly added</span>
                 </button>
               </div>
             </div>
