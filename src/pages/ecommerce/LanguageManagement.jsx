c00582c6302405aba75132845c01c4f22ae85ce4
nipaaa <akternipa941@gmail.com>
2023-03-09 16:39:49 -0800

    language and transact6ion currency add

--- src/pages/ecommerce/LanguageManagement.jsx
+++ src/pages/ecommerce/LanguageManagement.jsx
@@ -26,13 +26,55 @@ const LanguageManagement = () => {
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
             {/* Page header */}
-            <div className="sm:flex sm:justify-between sm:items-center mb-8">
+            <div className="sm:flex flex-wrap gap-8 sm:justify-between sm:items-center mb-8">
               {/* Left: Title */}
               <div className="mb-4 sm:mb-0">
                 <h1 className="text-2xl md:text-3xl text-slate-800 font-bold">
                   Language Management ✨
                 </h1>
               </div>
+              <div>
+                <label className="mr-2" for="status">
+                  Languages
+                </label>
+                <select className="border-none" name="status" id="status">
+                  <option>Please select a language</option>
+                  <option>A</option>
+                  <option>B</option>
+                  <option>C</option>
+                </select>
+              </div>
+              <div>
+                  <label className="mr-2" for="status">
+                    Configuration type
+                  </label>
+                  <select className="border-none" name="status" id="status">
+                    <option>Please select a configuration</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                  </select>
+                </div>
+                <div>
+                  <label className="mr-2" for="status">
+                    To configure key
+                  </label>
+                  <input
+                    className="border-none"
+                    type="text"
+                    placeholder="please enter the configure"
+                  />
+                </div>
+                <div>
+                  <label className="mr-2" for="status">
+                    Content
+                  </label>
+                  <input
+                    className="border-none"
+                    type="text"
+                    placeholder="please enter the content"
+                  />
+                </div>
 
               {/* Right: Actions */}
               <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
@@ -50,7 +92,7 @@ const LanguageManagement = () => {
                   >
                     <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                   </svg>
-                  <span className="hidden xs:block ml-2">Add Order</span>
+                  <span className="hidden xs:block ml-2">Newly added</span>
                 </button>
               </div>
             </div>
--- src/pages/ecommerce/TransactionCurrency.jsx
+++ src/pages/ecommerce/TransactionCurrency.jsx
@@ -33,16 +33,32 @@ const TransactionCurrency = () => {
                    Transaction Currency ✨
                   </h1>
                 </div>
+             
+              <div>
+                  <label className="mr-2" for="status">
+                   Chain
+                  </label>
+                  <select className="border-none" name="status" id="status">
+                    <option>Please select the main chain</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                  </select>
+                </div>
+                <div>
+                  <label className="mr-2" for="status">
+              Permission character
+                  </label>
+                  <input
+                    className="border-none"
+                    type="text"
+                    placeholder="please enter role name"
+                  />
+                </div>
   
                 {/* Right: Actions */}
                 <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-                  {/* Delete button */}
-                  {/* <DeleteButton selectedItems={selectedItems} /> */}
-                  {/* Dropdown */}
-                  <DateSelect />
-                  {/* Filter button */}
-                  {/* <FilterButton align="right" /> */}
-                  {/* Add customer button */}
+                
                   <button className="btn bg-indigo-500 hover:bg-indigo-600 text-white">
                     <svg
                       className="w-4 h-4 fill-current opacity-50 shrink-0"
@@ -50,7 +66,7 @@ const TransactionCurrency = () => {
                     >
                       <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                     </svg>
-                    <span className="hidden xs:block ml-2">Add Order</span>
+                    <span className="hidden xs:block ml-2">Newly added</span>
                   </button>
                 </div>
               </div>
