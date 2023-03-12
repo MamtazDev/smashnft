768001c428b54b9c24796d86caeaa3e5ca82b107
nipaaa <akternipa941@gmail.com>
2023-03-11 18:14:07 -0800

    update

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -1091,7 +1091,128 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                               </span>
                             </NavLink>
                           </li>
-                         
+
+                          {/* special */}
+                          <li className="mb-1 last:mb-0">
+                            <SidebarLinkGroup
+                              activecondition={
+                                pathname === "/" || pathname.includes("contract")
+                              }
+                            >
+                              {(handleClick, open) => {
+                                return (
+                                  <React.Fragment>
+                                    <a
+                                      href="#0"
+                                      className={`block text-slate-200 hover:text-white truncate transition duration-150 ${
+                                        (pathname === "/" ||
+                                          pathname.includes("contract")) &&
+                                        "hover:text-slate-200"
+                                      }`}
+                                      onClick={(e) => {
+                                        e.preventDefault();
+                                        sidebarExpanded
+                                          ? handleClick()
+                                          : setSidebarExpanded(true);
+                                      }}
+                                    >
+                                      <div className="flex items-center justify-between">
+                                        <div className="flex items-center">
+                                          <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                          Deploying the Contract
+                                          </span>
+                                        </div>
+                                        {/* Icon */}
+                                        <div className="flex shrink-0 ml-2">
+                                          <svg
+                                            className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${
+                                              open && "transform rotate-180"
+                                            }`}
+                                            viewBox="0 0 12 12"
+                                          >
+                                            <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                                          </svg>
+                                        </div>
+                                      </div>
+                                    </a>
+                                    <div className="lg:hidden lg:sidebar-expanded:block 2xl:block">
+                                      <ul
+                                        className={`pl-9 mt-1 ${
+                                          !open && "hidden"
+                                        }`}
+                                      >
+                                        <li className="mb-1 last:mb-0">
+                                          <NavLink
+                                            end
+                                            to="/contract/erc20"
+                                            className={({ isActive }) =>
+                                              "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
+                                              (isActive
+                                                ? "!text-indigo-500"
+                                                : "")
+                                            }
+                                          >
+                                            <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                            ERC20 contract deployment
+                                            </span>
+                                          </NavLink>
+                                        </li>
+                                        <li className="mb-1 last:mb-0">
+                                          <NavLink
+                                            end
+                                            to="/contract/erc21"
+                                            className={({ isActive }) =>
+                                              "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
+                                              (isActive
+                                                ? "!text-indigo-500"
+                                                : "")
+                                            }
+                                          >
+                                            <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                            ERC21 contract deployment
+                                            </span>
+                                          </NavLink>
+                                        </li>
+                                        <li className="mb-1 last:mb-0">
+                                          <NavLink
+                                            end
+                                            to="/home"
+                                            className={({ isActive }) =>
+                                              "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
+                                              (isActive
+                                                ? "!text-indigo-500"
+                                                : "")
+                                            }
+                                          >
+                                            <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                            Registry Contract Deployment
+                                            </span>
+                                          </NavLink>
+                                        </li>
+                                        <li className="mb-1 last:mb-0">
+                                          <NavLink
+                                            end
+                                            to="/home"
+                                            className={({ isActive }) =>
+                                              "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
+                                              (isActive
+                                                ? "!text-indigo-500"
+                                                : "")
+                                            }
+                                          >
+                                            <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                            Market Contract Deployment
+                                            </span>
+                                          </NavLink>
+                                        </li>
+                                      </ul>
+                                    </div>
+                                  </React.Fragment>
+                                );
+                              }}
+                            </SidebarLinkGroup>
+                          </li>
+
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
