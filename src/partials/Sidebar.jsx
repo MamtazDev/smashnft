82c26b5499a29f8bbe1d9a9deefdf6ae0e55e3cb
nipaaa <akternipa941@gmail.com>
2023-03-11 18:19:53 -0800

    update

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -1176,7 +1176,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                                         <li className="mb-1 last:mb-0">
                                           <NavLink
                                             end
-                                            to="/home"
+                                            to="/contract/wallet-reg-contract"
                                             className={({ isActive }) =>
                                               "block text-slate-400 hover:text-slate-200 transition duration-150 truncate " +
                                               (isActive
